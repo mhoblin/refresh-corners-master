1152bc1f00ca0e4b0ca60966601a676314f19dfc
mhoblin <monicahoblin@eagles.ewu.edu>
2014-10-20 13:24:01 -0700

    JS

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,8 @@ $(document).foundation();
 $(document).ready(function(){
   
   // Your Scripts
+  	$('#one').corner('20px');
+  	$('#two').corner('bevel 20px');
+  	$('#three').corner('notch 20px');
 
 });
